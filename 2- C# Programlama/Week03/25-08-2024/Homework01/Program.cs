0068b5d42a6531996e27b389800416f3b5fd44aa
tunacane <tunacaneski@gmail.com>
2024-08-25 20:35:35 +0300

    Homework1 / Switch örneğinin if hali

---
+++ 2- C# Programlama/Week03/25-08-2024/Homework01/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Homework01;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        System.Console.WriteLine("Sınav puanınızı giriniz: ");
+        int point = int.Parse(Console.ReadLine());
+        byte resultPoint = 0;
+
+        if (point < 45)
+        {
+            resultPoint = 1;
+        }
+        else if (point < 55)
+        {
+            resultPoint = 2;
+        }
+        else if (point < 70)
+        {
+            resultPoint = 3;
+        }
+        else if (point < 85)
+        {
+            resultPoint = 4;
+        }
+        else if (point <= 100)
+        {
+            resultPoint = 5;
+        }
+        else
+        {
+            System.Console.WriteLine("Geçerli bir not giriniz");
+        }
+
+        System.Console.WriteLine($"Sınav puanınız: {resultPoint}");
+    }
+}

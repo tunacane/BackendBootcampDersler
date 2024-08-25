aa2d8ba966cc1dcaac73c314c0cd614b9bc09a70
tunacane <tunacaneski@gmail.com>
2024-08-25 09:51:07 +0300

    24.08.2024 derste yapılan çalışmalar

---
+++ 2- C# Programlama/Week03/Project06_StringMethods/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Project06_StringMethods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //string fullName = "halil Umut MELER";
+
+        //int len = fullName.Length;
+        //System.Console.WriteLine(len);
+
+        // Console.Write(fullName.ToLower());
+        //System.Console.WriteLine(fullName.ToUpper());
+
+        // string adress1 = "Yeni Umut Sitesi E Blok";
+        // string adress2 = "No : 12 D:5";
+        // string distict =  "Kadıköy";
+        // string province = "İstanbul";
+
+        // string fullAdress1 = adress1+" "+adress2+" "+distict+" / "+province;
+        // string fullAdress2 = $"{adress1} {adress2} {distict}/{province}";
+        // System.Console.WriteLine(fullAdress1);
+        // System.Console.WriteLine(fullAdress2);
+
+        // string text1 = "Nişantaşı";
+        // string text2 = "Üniversitesi";
+        // string loc1 = "Bayrampaşa";
+        // string currentLoc = "Sarıyer";
+        // string date1 = "09.09.2009";
+        // string date2 = "05.05.2018";
+        // // Nişantaşı Üniversitesi 09.09.2009 tarihinde istanbul'un Bayrampaşa ilçesinde faaliyete başladı. Ardından 05.05.2018 tarihinde yine İstanbul'un Sarıyer ilçesinde kurulan TechCampus'e taşındı.
+
+        // string fullSentence1 = text1 +" "+ text2+" "+ date1+" tarihinde İstanbul'un "+loc1+ " ilçesinde faaliyete başladı. Ardından "+date2+" tarihinde yine İstanbul'un "+ currentLoc+ " ilçesinde kurulan TechCampus'e taşındı.";
+
+        // string fullSentence2 = $"{text1} {text2}, {date1} tarihinde İstanbul'un {loc1} ilçesinde faaliyete başladı. Ardından {date2} tarihinde yine İstanbul'un {currentLoc} ilçesinde kurulan TechCampus'e taşındı.";
+
+        // string fullSentence3 = string.Concat(text1," ",text2, ", ", date1, " tarihinde İstanbul'un", loc1, " ilçesinde faaliyete başladı. Ardından ", date2, " tarihinde yine İstanbul'un ", currentLoc, " ilçesinde kurulan TechCampus'e taşındı.");
+
+        // System.Console.WriteLine(fullSentence1);
+        // System.Console.WriteLine();
+        // System.Console.WriteLine(fullSentence2);
+        // System.Console.WriteLine();
+        // System.Console.WriteLine(fullSentence3);
+
+        // string fullName = "halil Umut MELER";
+        // string findText = "UmuT";
+        // bool response = fullName.ToLower().Contains(findText.ToLower());
+        // System.Console.WriteLine(response);
+        
+        // string firstName = "Begüm";
+        // System.Console.WriteLine(firstName.IndexOf("a"));
+
+        // string firstName = "Ayşen Umay";
+        // System.Console.WriteLine(firstName.ToLower().StartsWith("a"));
+        // System.Console.WriteLine(firstName.ToLower().EndsWith("ay"));
+
+        // string newsTitle = "Dolar'ın ateşi çıktı!";
+        // string newsID = "15454P5";
+        // string newsCategory = "Ekonomi";
+
+        // string newsUrl = newsTitle.ToLower();
+        
+        // newsUrl = newsUrl.Replace(" ", "-");
+        // newsUrl = newsUrl.Replace("'", "");
+        // newsUrl = newsUrl.Replace("!", "");
+        // newsUrl = newsUrl.Replace("ı", "i");
+        // newsUrl = newsUrl.Replace("ç", "c");
+        // newsUrl = newsUrl.Replace("ş", "s");
+
+        // newsUrl = $"{newsUrl}-{newsID}-{newsCategory.ToLower()}";
+
+        // System.Console.WriteLine(newsUrl);
+
+
+    }
+
+}

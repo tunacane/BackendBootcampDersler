a830e019c65d266d97cae10fb184c1ac3e4ef5a2
tunacane <tunacaneski@gmail.com>
2024-09-02 09:19:06 +0300

    Soru4 tamamlandı

---
+++ 2- C# Programlama/Week04/For_Soru4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace For_Soru4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Bir sayı giriniz: ");
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= number ; i++)
+        {
+            
+            for (int j = number ; j>0; j--)
+            {
+                System.Console.Write(" ");
+            }
+             for (int k = 1; k <= i+1; k++) {
+                System.Console.Write("*");
+            } 
+            System.Console.WriteLine();
+
+        }
+    }
+}

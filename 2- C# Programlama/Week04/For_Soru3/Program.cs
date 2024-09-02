9fe76033e12b10245630929c5e602ebba7b38c56
tunacane <tunacaneski@gmail.com>
2024-09-02 08:53:07 +0300

    Soru3 tamamlandı

---
+++ 2- C# Programlama/Week04/For_Soru3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace For_Soru3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Bir sayı giriniz: ");
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i < number; i++)
+        {
+            if (number%i==0){
+                System.Console.WriteLine($"{number}'ın tam bölenleri {i}'dir.");
+            }
+         
+           
+        }
+    }
+}

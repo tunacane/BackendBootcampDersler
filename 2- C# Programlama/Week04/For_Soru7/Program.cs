d2ed315d61d85588cd66304127cf0046a5e90193
tunacane <tunacaneski@gmail.com>
2024-09-03 14:48:15 +0300

    Soru7 tamamlandı

---
+++ 2- C# Programlama/Week04/For_Soru7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Xml.XPath;
+
+namespace For_Soru7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("10'dan büyük bir sayı giriniz: ");
+        int number = int.Parse(Console.ReadLine());
+        int result =0;
+
+        if (number < 10)
+        {
+            System.Console.WriteLine("Geçersiz veri girdiniz.");
+        }
+
+        for (int i = 1; i < number; i++)  
+             {
+            if (i %2 == 0)
+            {
+                result += i;
+                System.Console.WriteLine($"Sayıların toplamı: {result}");
+            }
+            
+        }
+    }
+}

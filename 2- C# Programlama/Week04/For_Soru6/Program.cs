abaa6e265a49c02df8fbbc2f8fcdf8b2f4684c0c
tunacane <tunacaneski@gmail.com>
2024-09-03 14:13:48 +0300

    Soru6 tamamlandı

---
+++ 2- C# Programlama/Week04/For_Soru6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.XPath;
+
+namespace For_Soru6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("10'dan büyük bir sayı giriniz: ");
+        int number = int.Parse(Console.ReadLine());
+        int result =0;
+
+        for (int i = 1; i < number; i++)
+        {
+
+            System.Console.WriteLine($"{i}^2 : {Math.Pow(i, 2)}");
+        }
+       
+    }
+}

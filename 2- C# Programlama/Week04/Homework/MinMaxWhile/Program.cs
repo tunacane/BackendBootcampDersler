1c301ec9a77ed1227c3d8f04767ecd3ae1e3ac75
tunacane <tunacaneski@gmail.com>
2024-09-04 15:10:48 +0300

    MaxMinWhile tamamlandı

---
+++ 2- C# Programlama/Week04/Homework/MinMaxWhile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+
+namespace MinMaxWhile;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+   
+      int maxNum = int.MinValue;
+      int minNum = int.MaxValue;
+
+       int i=0;
+
+       while (i<5)
+       {
+        System.Console.WriteLine("Bir sayı giriniz: ");
+        int num= int.Parse(Console.ReadLine());
+        i++;
+
+        if (num > maxNum)
+        {
+            maxNum = num;
+        }
+        if (num < minNum)
+        {
+            minNum =num;
+        }
+       }
+        System.Console.WriteLine($"Minimum sayı: {minNum}");
+        System.Console.WriteLine($"Maximum sayı: {maxNum}");
+    }
+}

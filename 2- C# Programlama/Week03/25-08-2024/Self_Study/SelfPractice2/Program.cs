f224168abda94aba31101d90aa8bc0f5ce8e9dfb
tunacane <tunacaneski@gmail.com>
2024-09-04 13:27:53 +0300

    While Soru8 tamamlandı

---
+++ 2- C# Programlama/Week03/25-08-2024/Self_Study/SelfPractice2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace SelfPractice2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        /*     ********************************************************
+        int sayi=0;
+        int sayiSon =0;
+
+        while (true)
+        {
+            System.Console.Write("Bir sayı giriniz: ");
+            sayi = int.Parse(Console.ReadLine());
+
+            System.Console.Write("Üst sınır sayı giriniz: ");
+            sayiSon = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine(sayi *= 2);
+
+            if (sayi > sayiSon)
+            {
+                break;
+            }
+        }  
+      
+        ***********************************************************      
+*/
+
+
+    }
+}
---
+++ 2- C# Programlama/Week04/Homework/While_Soru8/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace While_Soru8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number=1;
+        int numberCount = 0;
+        int count = 0;
+        double numOrt=0;
+
+
+        while (true)
+        {
+           System.Console.Write("Bir sayı giriniz: "); 
+           number = int.Parse(Console.ReadLine());
+           
+          if (number > 100)
+          {
+            break;
+
+          }
+
+            numberCount++;
+
+            count += number;
+             numOrt = count / numberCount;
+
+        }
+        System.Console.WriteLine(numberCount);
+        System.Console.WriteLine(count);
+        System.Console.WriteLine(numOrt);
+
+
+    }
+}

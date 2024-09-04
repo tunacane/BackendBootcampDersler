f067569c097ef8c29a52319b007afea92fac23cb
tunacane <tunacaneski@gmail.com>
2024-09-04 15:11:06 +0300

    MinMaxFor tamamlandı

---
+++ 2- C# Programlama/Week04/Homework/MinMaxFor/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MinMaxFor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int num;
+        int maxNum= int.MinValue;
+        int minNum = int.MaxValue;
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Bir sayı giriniz: ");
+             num = int.Parse(Console.ReadLine());
+          
+            if (num> maxNum)
+            {
+                maxNum = num;
+            }
+           if (num < minNum)
+           {
+            minNum = num;
+           }
+            
+        }
+        System.Console.WriteLine($"Minimum sayı: {minNum}");
+        System.Console.WriteLine($"Maximum sayı: {maxNum}");
+
+
+    }
+}

36c789ce7810df29fedaca620d307cd1f3484339
tunacane <tunacaneski@gmail.com>
2024-09-09 17:42:25 +0300

    Array Soru7 tamamlandı

---
+++ 2- C# Programlama/Week05/Homework/Array/Soru7/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Soru7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int[] numbers = new int[50];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = rnd.Next(1, 500);
+            System.Console.WriteLine(numbers[i] + " ");
+        }
+
+        System.Console.WriteLine();
+        
+        foreach (var even in numbers)
+        {
+            if (even%2==0)
+            {
+                System.Console.WriteLine(even+" ");
+            }
+        }
+
+    }
+}

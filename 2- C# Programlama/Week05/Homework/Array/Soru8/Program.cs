469856c756e8eecc480b4b73713e58a97173628e
tunacane <tunacaneski@gmail.com>
2024-09-09 20:43:37 +0300

    Array Soru8 tamamlandı

---
+++ 2- C# Programlama/Week05/Homework/Array/Soru8/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Soru8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        System.Console.WriteLine("Bir sayı giriniz: ");
+        int sayi = int.Parse(Console.ReadLine());
+
+        Random rnd = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = rnd.Next(1, 99);
+            System.Console.Write(numbers[i] + " ");
+        }
+
+        System.Console.WriteLine();
+
+        if (!numbers.Contains(sayi))
+        {
+            Array.Sort(numbers);
+
+            foreach (var number in numbers)
+            {
+                System.Console.Write(number+" ");
+
+            }
+System.Console.WriteLine();
+            
+
+            int[] newNumbers = new int[numbers.Length + 1];
+            Array.Copy(numbers, newNumbers, numbers.Length);
+            newNumbers[newNumbers.Length-1] = sayi;
+
+            Array.Sort(newNumbers);
+
+
+            foreach (var item in newNumbers)
+            {
+                System.Console.Write(item + " ");
+            }
+
+        }
+
+    }
+}
+
+// Array.Resize(ref numbers, numbers.Length+1);
+// int[] newNumbers = new int[11];
+// newNumbers[11] = sayi;
+
+// System.Console.WriteLine(newNumbers[i] + " ");

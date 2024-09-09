20e1056ad96804269c928c194b8068ac127236b0
tunacane <tunacaneski@gmail.com>
2024-09-09 21:41:03 +0300

    Array Soru10 tamamlandı

---
+++ 2- C# Programlama/Week05/Homework/Array/Soru9/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Soru9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { -2, 4, 5, -6, 8, 7, -9, 10, -11, 15, -48, 75, 42 };
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            System.Console.Write(numbers[i] + " ");
+
+            int[] positive = new int[numbers.Length];
+            Array.Reverse(positive);
+
+                        
+                foreach (var sayi in positive)
+                {
+                    if (sayi >0)
+                 {
+                     System.Console.WriteLine(sayi);
+                }
+                   
+                }
+        
+
+
+
+
+
+        }
+    }
+}

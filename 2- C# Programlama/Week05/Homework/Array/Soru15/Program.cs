1fb35307b39c1528028671db4ed3e057b76fc4d9
tunacane <tunacaneski@gmail.com>
2024-09-10 12:35:26 +0300

    Array Soru17 tamamlandı

---
+++ 2- C# Programlama/Week05/Homework/Array/Soru15/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Soru15;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 21, 3, 4, 5, 63, 7, 8, 12, 56, 47, 41, 15, 13, 78, 25 };
+       
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i]%2==1)
+            {
+                System.Console.Write(numbers[i]+" ");
+            }
+            Array.Sort(numbers);
+        }
+        int[] newNumbers = new int[numbers.Length];
+        
+    }
+}
---
+++ 2- C# Programlama/Week05/Homework/Array/Soru16/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Soru16;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        
+        int lastCount= int.MaxValue;
+       
+        int count=0;
+        int[] dizi = new int[lastCount];
+
+        do
+        {
+            System.Console.WriteLine("Bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            
+            count++;
+
+            if (sayi!=0)
+            {
+                dizi[count] = sayi;
+                count++;
+            }
+            else
+            {
+                Console.WriteLine("Dizi doldu. Daha fazla sayı giremezsiniz.");
+                break;
+            }
+
+
+        } while (true);
+
+
+        for (int i = 0; i < dizi.Length; i++)
+        {
+            dizi[i] = i;
+            System.Console.WriteLine(dizi[i]);
+        }
+
+
+
+
+    }
+}
+
+

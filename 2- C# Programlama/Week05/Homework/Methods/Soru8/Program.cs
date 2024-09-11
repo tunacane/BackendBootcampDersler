66694c7455692990a83eeb8d16b1aeae3c415310
tunacane <tunacaneski@gmail.com>
2024-09-11 18:35:43 +0300

    Methods Soru8 tamamlandı

---
+++ 2- C# Programlama/Week05/Homework/Methods/Soru8/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Soru8;
+/*Santigrat cinsinden verilen sıcaklığı Fahrenheit'e çeviren bir metot yazın.  °C × 1.8 + 32*/
+
+class Program
+{
+
+    static double Convert(double santigrat =30){
+
+       //santigrat = 30;
+        double fahrenheit = (santigrat *1.8)+32;
+        return fahrenheit;
+
+
+    }
+    static void Main(string[] args)
+    {
+        double fahrenheit= Convert();
+        System.Console.WriteLine($"30 Santigrat: {fahrenheit} F");
+    }
+}

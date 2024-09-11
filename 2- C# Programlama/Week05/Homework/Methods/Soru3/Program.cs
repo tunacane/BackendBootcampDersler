fd7a07fe5c5ddd099f3b05c7f577cb17a627fcc6
tunacane <tunacaneski@gmail.com>
2024-09-11 15:08:23 +0300

    Methods Soru3 tamamlandı

---
+++ 2- C# Programlama/Week05/Homework/Methods/Soru3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Soru3;
+/* Parametre olarak bir kelime alıp, bu kelimeyi tersine çevirerek geri döndüren bir metot yazın.**/
+
+class Program
+{
+
+    static string Reversed(string word){
+      
+        char[] array = word.ToCharArray();
+        Array.Reverse(array);
+
+        return  new string(array);
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        string word = "tunacan";
+
+        System.Console.WriteLine(Reversed(word));
+
+    }
+}

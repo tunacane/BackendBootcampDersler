9c200cbdf3812a91d5ce9f67a911bf90f9b9299c
tunacane <tunacaneski@gmail.com>
2024-09-11 14:54:26 +0300

    Methods Soru2 tamamlandı

---
+++ 2- C# Programlama/Week05/Homework/Methods/Soru2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Soru2;
+/* İki sayıyı karşılaştıran ve en büyüğünü döndüren bir metot yazın.*/
+
+
+class Program
+{
+    static int MinMax(int sayi1, int sayi2){
+
+        if (sayi1 > sayi2)
+        {
+            return sayi1;
+        }
+        else
+        {
+            return sayi2;
+        }
+
+    }
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine(MinMax(20, 10));
+    }
+}
